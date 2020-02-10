3eec66ba767299cf9bc6dbb79636d6fa626b8f46
Mathis Duret <mduret@esoftthings.com>
2020-02-10 11:32:58 +0100

    Add some reserved fields in metadata to simplify the next changes

--- include/eviewitf.h
+++ include/eviewitf.h
@@ -51,6 +51,7 @@ typedef struct {
     uint32_t frame_timestamp_lsb;
     uint32_t frame_timestamp_msb;
     uint32_t frame_sync;
+    uint32_t reserved[14];
 } eviewitf_frame_metadata_info_t;
 static const char* mfis_device_filenames[EVIEWITF_MAX_CAMERA] = {"/dev/mfis_cam0", "/dev/mfis_cam1", "/dev/mfis_cam2",
                                                                  "/dev/mfis_cam3", "/dev/mfis_cam4", "/dev/mfis_cam5",
