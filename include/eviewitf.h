3ea108650f649b0a437e9d5c35aacab277dfe489
Mathis Duret <mduret@esoftthings.com>
2020-02-10 10:33:33 +0100

    support new sync flag in frame metadata

--- include/eviewitf.h
+++ include/eviewitf.h
@@ -50,6 +50,7 @@ typedef struct {
     uint32_t frame_bpp;
     uint32_t frame_timestamp_lsb;
     uint32_t frame_timestamp_msb;
+    uint32_t frame_sync;
 } eviewitf_frame_metadata_info_t;
 static const char* mfis_device_filenames[EVIEWITF_MAX_CAMERA] = {"/dev/mfis_cam0", "/dev/mfis_cam1", "/dev/mfis_cam2",
                                                                  "/dev/mfis_cam3", "/dev/mfis_cam4", "/dev/mfis_cam5",
