9e01c89e5eb0c005683a520c9ec622c938a71a48
Benoît DESVAGES <bdesvages@esoftthings.com>
2020-04-17 11:48:45 +0200

    Add script to generate an ipk package

---
+++ scripts/build_ipk.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+source $(dirname "$(readlink -f "$0")")/version.sh
+
+BUILDDIR="${BUILDDIR:-build}"
+IPKDIR=${BUILDDIR}/ipk
+
+# (Re)create workin dirs
+rm -rf ${IPKDIR}
+mkdir -p ${IPKDIR}/{control,data}
+VERSIONNAME=$(getVersion)
+
+# Create debian-binary file
+echo 2.0 > ${IPKDIR}/debian-binary
+
+# Create control file
+echo Package: eViewItf > ${IPKDIR}/control/control
+echo Version: ${VERSIONNAME} >> ${IPKDIR}/control/control
+echo Architecture: ecube >> ${IPKDIR}/control/control
+echo Maintainer: eSoftThings >> ${IPKDIR}/control/control
+echo Description: This package contains the eViewItf libraries and binaries >> ${IPKDIR}/control/control
+echo Depends: kernel-module-eviewitf-mfis >> ${IPKDIR}/control/control
+
+# Populate data
+make install DESTDIR=${IPKDIR}/data
+
+# Build ipk (archive)
+pushd ${IPKDIR}/control
+tar --numeric-owner --group=0 --owner=0 -czf ../control.tar.gz ./*
+popd
+
+pushd ${IPKDIR}/data
+tar --numeric-owner --group=0 --owner=0 -czf ../data.tar.gz ./*
+popd
+
+pushd ${IPKDIR}
+tar --numeric-owner --group=0 --owner=0 -cf ../eviewitf-${VERSIONNAME}.ecube.ipk ./debian-binary ./data.tar.gz ./control.tar.gz 
+popd
