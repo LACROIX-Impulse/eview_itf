197734c3cee7f08f9d7dc4d74332e0b920f5f596
JANVIER Thomas <th.janvier@lacroix.group>
2022-02-23 09:41:53 +0100

    feature/EMIRROR-299-eviewitf-set-get-frame-rate-ioctl

--- src/cam-ioctl.h
+++ src/cam-ioctl.h
@@ -88,6 +88,8 @@ struct cam_dg {
 #define IOCSCAMOFFSET  MFIS_IOW(14, sizeof(struct cam_pt))  /* Sets the frame offset */
 #define IOCGCAMDG      MFIS_IOR(15, sizeof(struct cam_dg))  /* Gets the sensor CFA digital gains parameters */
 #define IOCSCAMDG      MFIS_IOW(16, sizeof(struct cam_dg))  /* Sets the sensor CFA digital gains parameters */
+#define IOCGCAMFPS     MFIS_IOR(17, sizeof(uint8_t))        /* Gets the sensor frame rate */
+#define IOCSCAMFPS     MFIS_IOW(18, sizeof(uint8_t))        /* Sets the sensor frame rate */
 #define IOCGCAMTP      MFIS_IOR(50, sizeof(uint8_t))        /* Gets the test pattern */
 #define IOCSCAMTP      MFIS_IOW(51, sizeof(uint8_t))        /* Sets the test pattern */
 #define IOCCAMREBOOT   MFIS_IO(100)                         /* Reboot command */
