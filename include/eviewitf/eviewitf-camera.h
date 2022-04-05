5c0cecc9a9faf6137016bed7b264f4877267ef9b
CHAUVIN François <f.chauvin@lacroix.group>
2022-04-05 17:30:50 +0200

    include/eviewitf/eviewitf-camera.h edited online with Bitbucket

--- include/eviewitf/eviewitf-camera.h
+++ include/eviewitf/eviewitf-camera.h
@@ -70,7 +70,7 @@ int eviewitf_camera_close(int cam_id);
 
 /**
  * \fn int eviewitf_camera_start(int cam_id)
- * \brief Request R7 to start camera, currently not exposed in libeviewitf
+ * \brief Request to start a camera
  *
  * \param[in] cam_id id of the camera between 0 and EVIEWITF_MAX_CAMERA
  * \return return code as specified by the eviewitf_return_code enumeration.
